6bd18ed9463817dc34a943ca38ca16b7232076a6
Dharm Raj Hembram <dharmrajh03@gmail.com>
2023-04-07 11:21:30 +0530

    first commit

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+//const bcrypt = require("bcryptjs");
+//const jwt = require("jsonwebtoken");
+//const expenseRouter = require('./routes/expenseRoutes');
+const userRouter=require('./routes/userRoutes');
+const cors = require("cors");
+const Razorpay = require('razorpay');
+const User = require("./models/user");
+const Expense = require("./models/expense");
+const expenseRoutes=require('./routes/expenseRoutes');
+
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+//app.use('/', expenseRouter);
+app.use('/',userRouter);
+app.use('/',expenseRoutes);
+app.listen(8000, () => {
+    console.log("port is listening on 8000");
+})
